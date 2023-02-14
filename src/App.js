993bf989add8f866ea820d4b090cda43f5ef104c
jospabcor <jospabcor@gmail.com>
2023-02-14 11:59:08 -0700

    First version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
+//import './App.css';
+
+const todos = [
+  {text: 'cortar cebolla', completed:false},
+  {text: 'Tomar el curso de intro de react', completed:false},
+  {text: 'Llorar con la loorona', completed:false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <TodoCounter/>
+    <TodoSearch/>
+
+    <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+        </TodoList>
+      <CreateTodoButton/>
+      
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoSearch(){
+    return(
+        
+            <input placeholder="cebolla"/>
+        
+    );
+}
+
+export { TodoSearch };
